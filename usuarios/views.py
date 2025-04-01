1b148af23587d724892c85de519e2bd8e1cc95d5
pedromakaveli <pedrolacerda804@gmail.com>
2025-04-01 01:40:41 +0000

    Captura os dados da requisição em cadastro

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,7 +3,13 @@ from django.http import HttpResponse
 # Create your views here.
 
 def cadastro (request):
-    return render(request, "cadastro.html")
+    
+    if request.method == "GET":
+        return render(request, "cadastro.html")
+    elif request.method == "POST":
+        dados = request.body()
+        print(dados)
+        return dados
 
 def login (request):
     return HttpResponse("Página de Login")
