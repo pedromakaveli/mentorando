3bb8edc60ce670f5144ad4f90d0776a06536b123
pedromakaveli <pedrolacerda804@gmail.com>
2025-04-02 01:13:50 +0000

    Adiciona mensagens de erro e sucesso

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
+
 # Create your views here.
 
 def cadastro (request):
@@ -21,12 +24,15 @@ def cadastro (request):
         users = User.objects.filter(username=username)
         
         if not senha == confirmar_senha:
-            return render(request, "cadastro.html", context={"msg": "As senhas não conferem"})
+            messages.add_message(request, constants.ERROR, "Senha e confirmar senha devem ser iguais.")
+            return redirect("cadastro")
         
         elif len(senha) < 6:
+            messages.add_message(request, constants.ERROR, "A senha deve ter 6 ou mais caracteres.")
             return redirect("cadastro")
         
         elif users.exists():
+            messages.add_message(request, constants.ERROR, "Já existe um usuário com esse username.")
             return redirect("cadastro")
         
         else:
@@ -35,7 +41,7 @@ def cadastro (request):
                 password=senha
             )
             
-            return redirect ("/usuarios/login")
+            return redirect("login")
 
 def login (request):
     return HttpResponse("Página de Login")
