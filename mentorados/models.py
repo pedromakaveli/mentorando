9985f770ca3eef08228b2c67b00db2224c6f1b0e
pedromakaveli <pedrolacerda804@gmail.com>
2025-04-03 23:43:11 +0000

    Adiciona tabelas para o app mentorados

--- mentorados/models.py
+++ mentorados/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Navigators (models.Model):
+    nome = models.CharField(max_length=255)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__ (self):
+        return self.nome
 
 # Create your models here.
+class Mentorados (models.Model):
+    estagio_choices = (
+        ('E1', '10-100k'),
+        ('E2', '100-1kk')
+    )
+    
+    nome = models.CharField(max_length=255)
+    foto = models.ImageField(upload_to='fotos', null=True, blank=True)
+    estagio = models.CharField(max_length=2, choices=estagio_choices)
+    navigator = models.ForeignKey(Navigators, null=True, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    criado_em = models.DateField(auto_now_add=True)
+    
+    def __str__ (self):
+        return self.nome
+    
