ef250ff03369f804b81ce5bd3c1204e2e6f0ea1b
pedromakaveli <pedrolacerda804@gmail.com>
2025-04-04 00:26:24 +0000

    Exibe os mentorados e os estagios cadastrados no db diretamente no front

--- mentorados/views.py
+++ mentorados/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Mentorados
+from .models import Navigators
 # Create your views here.
 
 def mentorados (request):
     if request.method == "GET":
-        return render(request, 'mentorados.html')
+        navigators = Navigators.objects.all()
+        return render(request, 'mentorados.html', {'estagios': Mentorados.estagio_choices, 'navigators': navigators})
     elif request.method == "POST":
         ...
