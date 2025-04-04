066fe5dc5da8af6be9814518cbbb3b183e4310da
pedromakaveli <pedrolacerda804@gmail.com>
2025-04-04 00:10:15 +0000

    Verifica metodo da requisicao em mentorados

--- mentorados/views.py
+++ mentorados/views.py
@@ -3,4 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def mentorados (request):
-    return render(request, 'mentorados.html')
+    if request.method == "GET":
+        return render(request, 'mentorados.html')
+    elif request.method == "POST":
+        ...
